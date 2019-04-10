c6c4ede793c7d67f8d8bf3498e52b0325ba751fd
Sam Bogoslavets <sam_1997@mail.ru>
2019-04-10 19:29:20 +0300

    Update 0.2

---
+++ app/src/main/java/com/example/androidacademyproject/AuthorActivity.java
@@ -0,0 +1,13 @@
+package com.example.androidacademyproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AuthorActivity extends Activity {
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_author);
+    }
+}
--- app/src/main/java/com/example/androidacademyproject/MainActivity.java
+++ app/src/main/java/com/example/androidacademyproject/MainActivity.java
@@ -1,12 +1,22 @@
 package com.example.androidacademyproject;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener{
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.bt_author).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.bt_author)
+            startActivity(new Intent(this, AuthorActivity.class));
     }
 }

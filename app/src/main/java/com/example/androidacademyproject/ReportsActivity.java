84368c0fee9f793f34c9ca77ff7212dbea94514c
Sam Bogoslavets <sam_1997@mail.ru>
2019-05-17 12:48:11 +0300

    Fix author and report activities (add flatButton)

--- app/src/main/java/com/example/androidacademyproject/ReportsActivity.java
+++ app/src/main/java/com/example/androidacademyproject/ReportsActivity.java
@@ -2,10 +2,9 @@ package com.example.androidacademyproject;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.util.Log;
 
 import com.example.androidacademyproject.activity.AuthorActivity;
